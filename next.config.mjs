a70b983ad8ebc1031cbf089ade63a0ef60949261
kanu123 <kanu0311@gmail.com>
2025-05-10 00:34:16 +0530

    third commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['images.unsplash.com', 'res.cloudinary.com']
-    }
+  images: {
+    domains: ['images.unsplash.com', 'res.cloudinary.com']
+  },
+  eslint: {
+    // Disable specific ESLint rules for the project
+    ignoreDuringBuilds: true, // Ignore ESLint during build (use with caution)
+  }
 };
 
 export default nextConfig;
